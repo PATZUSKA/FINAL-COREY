680f335b26875bf29bca6a06dacaf6ff016123b5
PATZUSKA <133899944+PATZUSKA@users.noreply.github.com>
2025-02-14 17:27:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,24 +12,13 @@ document.getElementById("no").addEventListener("mouseover", function() {
     this.style.top = `${y}px`;
 });
 
-// Fonction pour afficher les messages des lettres
-function showMessage(type) {
-    let messages = {
-        "sad": "I don't know what you're going through. But, just remember: You don't have to be perfect. It's okay to be having a bad day. Life goes on. Let's get through this together.",
-        "weak": "I'm proud of you. Most people don't know your story, but I do. You're strong, and I'm proud of the man you are becoming.",
-        "love": "I like you without a reason. If love had a reason, it would fade when the reason disappears. I love you because you are you.",
-        "stupid": "WHY DID YOU CLICK ON THAT? YOU’RE NOT DUMB AYO!"
-    };
-    alert(messages[type]);
-}
-
-// Placement aléatoire des bisous
+// Placement aléatoire des chats
 document.addEventListener("DOMContentLoaded", function() {
-    let kisses = document.querySelectorAll(".kiss");
-    kisses.forEach(kiss => {
+    let cats = document.querySelectorAll(".cat");
+    cats.forEach(cat => {
         let x = Math.random() * window.innerWidth;
         let y = Math.random() * window.innerHeight;
-        kiss.style.left = `${x}px`;
-        kiss.style.top = `${y}px`;
+        cat.style.left = `${x}px`;
+        cat.style.top = `${y}px`;
     });
 });
