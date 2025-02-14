814fce79ae3d427f82a4e9bfa3a400767dfbb1b5
PATZUSKA <133899944+PATZUSKA@users.noreply.github.com>
2025-02-14 19:59:29 +0100

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,10 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Attach click event listeners to each message
-    let messages = document.querySelectorAll('.message');
-    messages.forEach(message => {
-        message.addEventListener('click', function() {
-            let text = message.querySelector('.text');
-            text.style.display = 'block';  // Show text when the letter is clicked
-        });
-    });
-});
+.message {
+    text-align: center;
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.message img {
+    width: auto;  /* Ajuste la largeur en fonction du texte */
+    max-width: 100%; /* Empêche l'image d'être plus large que l'écran */
+    height: auto; /* Conserve les proportions */
+}
+
+h2 {
+    font-size: 1.2rem;
+    width: auto;
+    text-align: center;
+}
+
+.text {
+    display: none;
+    font-size: 1rem;
+    color: #555;
+    margin-top: 10px;
+    max-width: 90%; /* Pour éviter que le texte soit trop large */
+    word-wrap: break-word; /* Permet au texte de bien s’adapter */
+    text-align: center;
+}
+
+.message:hover .text {
+    display: block;
+}
